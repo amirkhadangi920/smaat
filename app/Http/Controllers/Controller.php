51086e3756769360e4f8f7b9ee4e6c14130e9631
Amir Khadangi <AmirKhadangi920@gmail.com>
2019-03-05 17:55:45 +0330

    Create upload image method on base controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Traits\ImageTools;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ImageTools;
+
+    public function requestWithImage ($request, $field_name = 'logo' )
+    {
+        return collect( array_merge( $request->except($field_name), [
+            $field_name => $this->upload_image( $request->file( $field_name ) )
+        ]) );
+    }
 }
