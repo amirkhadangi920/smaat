280435e3941438703d2c748539369eb070384ecf
Amir Khadangi <AmirKhadangi920@gmail.com>
2019-03-06 13:43:11 +0330

    Fix a bug on upload image method & checkPermission method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,10 +12,31 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ImageTools;
 
-    public function requestWithImage ($request, $field_name = 'logo' )
+    /**
+     * Get a request with a file and upload it's file,
+     * then return the same request with uploaded file names
+     *
+     * @param Request $request
+     * @param string $field_name
+     * @param Model $model
+     * @return Request
+     */
+    public function requestWithImage($request, $field_name = 'logo', $model = null)
     {
+        if ( !$request->hasFile($field_name) )
+            return $request;
+
+        if ( $model && file_exists( public_path($model->$field_name) ) )
+                unlink( public_path($model->$field_name) );
+
         return collect( array_merge( $request->except($field_name), [
             $field_name => $this->upload_image( $request->file( $field_name ) )
         ]) );
+    }
+
+    public function checkPermission(string $permission)
+    {
+        if ( !auth()->check() || !auth()->user()->can($permission) )
+            return abort(403);
     }
 }
