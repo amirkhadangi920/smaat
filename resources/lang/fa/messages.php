35cc8742881517ae69f26dd27e46d36b8e4f1bd6
Amir Khadangi <AmirKhadangi920@gmail.com>
2019-03-06 13:43:46 +0330

    Add Fa_ir lang files

---
+++ resources/lang/fa/messages.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Response messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in controllers for various
+    | messages that we need to return to the api.
+    |
+    */
+
+    'return' => [
+        'all'       => 'لیست تمام :data ها',
+        'single'    => 'اطلاعات :data مورد نظر',
+        'paginate'  => 'بخشی از :data ها'
+    ],
+    'create' => [
+        'successful' => ':data مورد نظر با موفقیت ثبت شد'
+    ],
+    'update' => [
+        'successful' => ':data مورد نظر با موفقیت بروزرسانی شد'
+    ],
+    'delete' => [
+        'successful' => ':data مورد نظر با موفقیت حذف شد',
+        'plural' => ':data های مورد نظر با موفقیت حذف شدند'
+    ],
+];
---
+++ resources/lang/fa/types.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Data type Titles Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in controllers for determine
+    | the title of response messages that we need to return to the api.
+    |
+    */
+
+    'category' => [
+        'title' => 'دسته بندی',
+    ]
+];
