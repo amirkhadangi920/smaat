d902ebe4af312f8c1fe80084498e0656a2eda9ff
Amir Khadangi <AmirKhadangi920@gmail.com>
2019-03-06 13:45:48 +0330

    Fix a laratrust user_id field problem and change project lang to fa

--- database/migrations/2019_03_02_134412_laratrust_setup_tables.php
+++ database/migrations/2019_03_02_134412_laratrust_setup_tables.php
@@ -32,7 +32,7 @@ class LaratrustSetupTables extends Migration
         // Create table for associating roles to users and teams (Many To Many Polymorphic)
         Schema::create('role_user', function (Blueprint $table) {
             $table->unsignedInteger('role_id');
-            $table->unsignedInteger('user_id');
+            $table->uuid('user_id');
             $table->string('user_type');
 
             $table->foreign('role_id')->references('id')->on('roles')
@@ -44,7 +44,7 @@ class LaratrustSetupTables extends Migration
         // Create table for associating permissions to users (Many To Many Polymorphic)
         Schema::create('permission_user', function (Blueprint $table) {
             $table->unsignedInteger('permission_id');
-            $table->unsignedInteger('user_id');
+            $table->uuid('user_id');
             $table->string('user_type');
 
             $table->foreign('permission_id')->references('id')->on('permissions')
