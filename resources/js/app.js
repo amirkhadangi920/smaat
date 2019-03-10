a15b71de246870251e4cce1800c50e1a78966b98
simonsalamon <salamon.1378@gmail.com>
2019-03-11 00:22:30 +0330

    install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App-start.vue";
 
-import './bootstrap';
+Vue.component('App-s' , App);
+// router setup
+import routes from "./routes/routes";
 
+// Plugins
+import GlobalComponents from "./globalComponents";
+import GlobalDirectives from "./globalDirectives";
+import Notifications from "./components/NotificationPlugin";
+import axios from 'axios';
+// MaterialDashboard plugin
+import MaterialDashboard from "./material-dashboard";
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import Chartist from "chartist";
 
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkExactActiveClass: "nav-item active"
+});
 
-const app = new Vue({
-    el: '#app'
+Vue.prototype.$Chartist = Chartist;
+
+Vue.use(VueRouter);
+Vue.use(MaterialDashboard);
+Vue.use(GlobalComponents);
+Vue.use(GlobalDirectives);
+Vue.use(Notifications);
+Vue.use(axios);
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  render: h => h(App),
+  router,
+  data: {
+    Chartist: Chartist
+  }
 });
