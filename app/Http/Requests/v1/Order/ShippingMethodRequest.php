17890005ab98713e9e93ecc9044ca0bd674acbb5
Mrzarabi <hadi.z.1997@gmail.com>
2019-03-11 17:17:22 +0330

    make Request Shipping Method

---
+++ app/Http/Requests/v1/Order/ShippingMethodRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\v1\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShippingMethodRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'              => 'required|string|min:50',
+            'description'       => 'nullable|string|min:255',
+            'logo'              => 'nullable|image|mimes:jpeg,jpg,png,gif|max:1024',
+            'cost'              => 'required|digit|min:0',
+            'minimum'           => 'required|digit|min:0',
+            'is_active'         => 'required|boolean'
+        ];
+    }
+}
