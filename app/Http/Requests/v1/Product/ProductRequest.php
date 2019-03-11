dec978848b2b7ca49471a415fdcf4abfc1436be7
Mrzarabi <hadi.z.1997@gmail.com>
2019-03-11 20:23:31 +0330

    refix bugs Request product

--- app/Http/Requests/v1/Product/ProductRequest.php
+++ app/Http/Requests/v1/Product/ProductRequest.php
@@ -24,19 +24,28 @@ class ProductRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'          => 'nullable|max:50|string',
-            'second_name'   => 'nullable|max:50|string',
-            'code'          => ['nullable', 'regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/'],
-            'description'   => 'nullable|max:255|string',
-            'note'          => 'nullable|max:255|string',
-            // 'aparat_video'  => 'nullable|' ,
-            'status'        => 'nullable|boolean|digits_between:0,127',
-            'review'        => 'nullable|string',
-            'advantages'    => 'required|string|array',
-            'disadvantages' => 'required|string|array',
-            'label'         => 'nullable|digit',
-            'views_count'   => 'required|digits_between:0,1000000',
-            'photos'        => 'nullable|image|mimes:jpeg,jpg,png,gif|max:1024'
+            'name'              => 'nullable|string|max:50',
+            'second_name'       => 'nullable|string|max:50',
+            'code'              => ['nullable', 'regex:/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/'],
+            'description'       => 'nullable|max:255|string',
+            'note'              => 'nullable|max:255|string',
+            // 'aparat_video'   => 'nullable|' ,
+            'status'            => 'nullable|boolean|digits_between:0,127',
+            'review'            => 'nullable|string',
+            'keywords'          => 'required|array|string',  
+            'photos'            => 'nullable|image|mimes:jpeg,jpg,png,gif|max:1024',
+            'advantages'        => 'required|string|array',
+            'disadvantages'     => 'required|string|array',
+            'label'             => 'nullable|digit',
+            'views_count'       => 'required|digits_between:0,1000000',
+
+            /**
+             * relateion 
+             */
+            'users.*'           => 'required|array|exists:users,id',
+            'categories.*'      => 'required|integer|exists:categories,id',
+            'specs.*'           => 'required|integer|exists:speces,id',  
+            'accessories.*'     => 'required|integer|exists:product,id',  
         ];
     }
 }
