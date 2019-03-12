a1a05b237d8d599511dfd40f6729ea80a67d5db2
Mrzarabi <hadi.z.1997@gmail.com>
2019-03-12 17:26:20 +0330

    make persian and fix some bugs

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -1,18 +1,30 @@
 <?php
 
-use Faker\Generator as Faker;
+use Faker\Factory;
 
+$faker = Factory::create('fa_IR');
 
 function nullable ( $value )
 {
     return [ null, $value ][ rand(0, 1) ];
 }
 
-$factory->define(App\Models\Article::class, function (Faker $faker) {
+function image ($file)
+{
+    return [
+        'big'       => $file,
+        'medium'    => $file,
+        'small'     => $file,
+        'tiny'      => $file,
+    ];
+}
+
+
+$factory->define(App\Models\Article::class, function () use ($faker) {
     return [
         'title'         => $faker->name(),
-        'description'   => $faker->text(255),
-        'body'          => $faker->paragraph(),
+        'description'   => nullable( Faker::sentence(250) ),
+        'body'          => Faker::paragraph(),
         'image'         => $faker->imageUrl($width = 640, $height = 480),
         'reading_time'  => rand(1, 50)
     ];
